02c354cef1bdb0cfb864545114f635c0e27800b6
Lukas <lukasjp11@gmail.com>
2023-10-19 11:50:08 +0200

    fixes

--- js/script.js
+++ js/script.js
@@ -1,130 +1,142 @@
-// Global variables
-let currentDate = new Date();
-let selectedDates = []; // To store the selected dates and persons
-let clickedDate; // To store the clicked date
+const dinnerDeltagApp = {
+    currentDate: new Date(),
+    selectedDates: [],
+    clickedDate: null,
 
-// Function to update the calendar based on the current date
-function updateCalendar() {
-    const monthName = document.getElementById('monthName');
-    const calendarBody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
+    init() {
+        this.updateCalendar();
+    },
 
-    // Set the month name
-    const monthNames = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
-    monthName.innerText = monthNames[currentDate.getMonth()] + ' ' + currentDate.getFullYear();
+    updateCalendar() {
+        this.setMonthName();
+        this.clearPreviousCalendar();
+        this.createCalendar();
+        this.highlightToday();
+        this.highlightSelectedDates();
+        this.attachClickEventToCells();
+    },
 
-    // Clear the previous calendar
-    calendarBody.innerHTML = '';
+    setMonthName() {
+        const monthName = document.getElementById('monthName');
+        const monthNames = ['Januar', 'Februar', 'Marts', 'April', 'Maj', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'December'];
+        monthName.innerText = `${monthNames[this.currentDate.getMonth()]} ${this.currentDate.getFullYear()}`;
+    },
 
-    // Get the first day of the month
-    let firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
-    firstDay = (firstDay === 0) ? 6 : firstDay - 1; // Adjust Sunday to be at the end
+    clearPreviousCalendar() {
+        const calendarBody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
+        calendarBody.innerHTML = '';
+    },
 
-    // Get the total number of days in the month
-    const totalDays = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    createCalendar() {
+        const calendarBody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
+        let firstDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1).getDay();
+        firstDay = (firstDay === 0) ? 6 : firstDay - 1; // Adjust Sunday to be at the end
+        const totalDays = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0).getDate();
+        let dayCounter = 1;
 
-    // Create the calendar
-    let dayCounter = 1;
-    for (let i = 0; i < 6; i++) {
-        const row = document.createElement('tr');
+        for (let i = 0; i < 6; i++) {
+            const row = document.createElement('tr');
 
-        for (let j = 0; j < 7; j++) {
-            const cell = document.createElement('td');
+            for (let j = 0; j < 7; j++) {
+                const cell = document.createElement('td');
 
-            if (i === 0 && j < firstDay) {
-                const prevMonthLastDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-                cell.innerText = prevMonthLastDay - firstDay + j + 1;
-                cell.classList.add('grayed-out');
-            } else if (dayCounter > totalDays) {
-                cell.innerText = dayCounter - totalDays;
-                cell.classList.add('grayed-out');
-                dayCounter++;
-            } else {
-                cell.innerText = dayCounter;
-                dayCounter++;
+                if (i === 0 && j < firstDay) {
+                    const prevMonthLastDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 0).getDate();
+                    cell.innerText = prevMonthLastDay - firstDay + j + 1;
+                    cell.classList.add('grayed-out');
+                } else if (dayCounter > totalDays) {
+                    cell.innerText = dayCounter - totalDays;
+                    cell.classList.add('grayed-out');
+                    dayCounter++;
+                } else {
+                    cell.innerText = dayCounter;
+                    dayCounter++;
+                }
+
+                row.appendChild(cell);
             }
 
-            row.appendChild(cell);
+            calendarBody.appendChild(row);
         }
+    },
 
-        calendarBody.appendChild(row);
-    }
-
-    // Highlight the selected dates and show the selected person and guest status
-    highlightSelectedDates();
-
-    // After creating the calendar, attach the click event listeners to the cells
-    attachClickEventToCells();
-}
-
-// Function to highlight the selected dates
-function highlightSelectedDates() {
-    selectedDates.forEach(dateInfo => {
-        if (dateInfo.year === currentDate.getFullYear() && dateInfo.month === currentDate.getMonth()) {
-            const cell = document.querySelector(`#calendar tbody tr:nth-child(${Math.ceil((dateInfo.date + dateInfo.firstDay) / 7)}) td:nth-child(${(dateInfo.date + dateInfo.firstDay - 1) % 7 + 1})`);
-            if (cell) {
-                cell.style.backgroundColor = 'green';
-                cell.style.color = 'white';
-                cell.innerHTML = `<strong>${dateInfo.date}</strong>
-                                  <div>${dateInfo.person}<br>${dateInfo.guest ? 'Med gæster' : ''}</div>`;
+    highlightToday() {
+        const today = new Date();
+        if (today.getMonth() === this.currentDate.getMonth() && today.getFullYear() === this.currentDate.getFullYear()) {
+            const firstDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1).getDay();
+            const adjustedFirstDay = (firstDay === 0) ? 6 : firstDay - 1;
+            const todayCell = document.querySelector(`#calendar tbody tr:nth-child(${Math.ceil((today.getDate() + adjustedFirstDay) / 7)}) td:nth-child(${(today.getDate() + adjustedFirstDay - 1) % 7 + 1})`);
+            
+            if (todayCell) {
+                todayCell.classList.add('today');
             }
         }
-    });
-}
+    },
 
-
-// Function to change the month
-function changeMonth(direction) {
-    currentDate.setMonth(currentDate.getMonth() + direction);
-    updateCalendar();
-}
-
-// Function to open the modal
-function openModal() {
-    document.getElementById("modal").style.display = "block";
-}
-
-// Function to close the modal and capture the selected values
-function closeModalAndSave() {
-    const selectedName = document.getElementById("names").value;
-    const isGuest = document.getElementById("guest").checked;
-
-    const firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
-    const adjustedFirstDay = (firstDay === 0) ? 6 : firstDay - 1;
-
-    const selectedDateInfo = {
-        date: clickedDate,
-        month: currentDate.getMonth(),
-        year: currentDate.getFullYear(),
-        person: selectedName,
-        guest: isGuest,
-        firstDay: adjustedFirstDay
-    };
-
-    // Update selectedDates array
-    selectedDates = selectedDates.filter(dateInfo => dateInfo.date !== clickedDate || dateInfo.month !== currentDate.getMonth() || dateInfo.year !== currentDate.getFullYear());
-    selectedDates.push(selectedDateInfo);
-
-    updateCalendar();
-    document.getElementById("modal").style.display = "none";
-}
-
-// Function to close the modal without saving the changes
-function closeModalWithoutSaving() {
-    document.getElementById("modal").style.display = "none";
-}
-
-
-// Function to attach click event to calendar cells
-function attachClickEventToCells() {
-    document.querySelectorAll('#calendar tbody td').forEach(cell => {
-        cell.addEventListener('click', () => {
-            if (!cell.classList.contains('grayed-out')) {
-                clickedDate = parseInt(cell.innerText, 10);
-                openModal();
+    highlightSelectedDates() {
+        this.selectedDates.forEach(dateInfo => {
+            if (dateInfo.year === this.currentDate.getFullYear() && dateInfo.month === this.currentDate.getMonth()) {
+                const cell = document.querySelector(`#calendar tbody tr:nth-child(${Math.ceil((dateInfo.date + dateInfo.firstDay) / 7)}) td:nth-child(${(dateInfo.date + dateInfo.firstDay - 1) % 7 + 1})`);
+                
+                if (cell) {
+                    cell.style.backgroundColor = 'green';
+                    cell.style.color = 'white';
+                    cell.innerHTML = `<strong>${dateInfo.date}</strong><div>${dateInfo.person}<br>${dateInfo.guest ? 'Med gæster' : ''}</div>`;
+                }
             }
         });
-    });
-}
+    },
 
-// Initial calendar update
-updateCalendar();
+    changeMonth(direction) {
+        this.currentDate.setMonth(this.currentDate.getMonth() + direction);
+        this.updateCalendar();
+    },
+
+    openModal() {
+        document.getElementById("modal").style.display = "block";
+        document.getElementById("guest").checked = false;
+    },
+
+    closeModalAndSave() {
+        const selectedName = document.getElementById("names").value;
+        const isGuest = document.getElementById("guest").checked;
+        const firstDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1).getDay();
+        const adjustedFirstDay = (firstDay === 0) ? 6 : firstDay - 1;
+
+        const selectedDateInfo = {
+            date: this.clickedDate,
+            month: this.currentDate.getMonth(),
+            year: this.currentDate.getFullYear(),
+            person: selectedName,
+            guest: isGuest,
+            firstDay: adjustedFirstDay
+        };
+
+        this.selectedDates = this.selectedDates.filter(dateInfo => dateInfo.date !== this.clickedDate || dateInfo.month !== this.currentDate.getMonth() || dateInfo.year !== this.currentDate.getFullYear());
+        this.selectedDates.push(selectedDateInfo);
+
+        this.updateCalendar();
+        document.getElementById("modal").style.display = "none";
+    },
+
+    closeModalWithoutSaving() {
+        document.getElementById("modal").style.display = "none";
+    },
+
+    attachClickEventToCells() {
+        document.querySelectorAll('#calendar tbody td').forEach(cell => {
+            cell.addEventListener('click', () => {
+                if (!cell.classList.contains('grayed-out')) {
+                    this.clickedDate = parseInt(cell.innerText, 10);
+                    this.openModal();
+                }
+            });
+        });
+    }
+};
+
+dinnerDeltagApp.init();
+
+window.changeMonth = dinnerDeltagApp.changeMonth.bind(dinnerDeltagApp);
+window.closeModalWithoutSaving = dinnerDeltagApp.closeModalWithoutSaving.bind(dinnerDeltagApp);
+window.closeModalAndSave = dinnerDeltagApp.closeModalAndSave.bind(dinnerDeltagApp);
