dd5da6e212ea4cf4b8a572278cddbce7984f86e0
Lukas <lukasjp11@gmail.com>
2023-11-11 14:16:27 +0100

    Revert "Merge pull request #9 from lukasjp11/dev"
    This reverts commit 956022b7839eacaab2ab322d72623d0a2c6419d8, reversing
    changes made to 282de87613baafaf09b1d5b83949ca670ffc7b54.

--- js/firebaseInit.js
+++ js/firebaseInit.js
@@ -3,13 +3,13 @@ import { getAuth, onAuthStateChanged, signInAnonymously } from 'https://www.gsta
 import { getDatabase, ref, onValue, set } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    apiKey: "AIzaSyBPFLXo6mQXdcszXFVYaD-aCq-hRNmRjkw",
+    authDomain: "dinnerdeltag.firebaseapp.com",
+    projectId: "dinnerdeltag",
+    storageBucket: "dinnerdeltag.appspot.com",
+    messagingSenderId: "1390491301",
+    appId: "1:1390491301:web:e4e61c46ed0ab5f7d5bf4c",
+    databaseURL: "https://dinnerdeltag-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 const app = initializeApp(firebaseConfig);
