d4d73b1514ea72a29ee93e22afc6c9f11c603722
Lukas <lukasjp11@gmail.com>
2024-11-08 22:16:06 +0100

    fix

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,17 +2,17 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/calendarStyles.css',
-                '/css/modalStyles.css',
-                '/js/app.js',
-                '/js/calendar.js',
-                '/js/modal.js',
-                '/js/firebaseInit.js',
-                '/images/DinnerDeltag-logo-full.png',
-                '/images/DinnerDeltag-logo-small.png',
-                '/manifest.json'
+                './',
+                './index.html',
+                './css/calendarStyles.css',
+                './css/modalStyles.css',
+                './js/app.js',
+                './js/calendar.js',
+                './js/modal.js',
+                './js/firebaseInit.js',
+                './images/DinnerDeltag-logo-full.png',
+                './images/DinnerDeltag-logo-small.png',
+                './manifest.json'
             ]);
         })
     );
